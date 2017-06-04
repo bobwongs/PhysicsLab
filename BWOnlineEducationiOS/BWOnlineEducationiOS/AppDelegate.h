e3084491265669cd543cb8ccef4a540e793ccc36
WangBo <wangbo@bluemoon.com.cn>
2017-06-04 22:48:19 +0800

    Upload project

---
+++ BWOnlineEducationiOS/BWOnlineEducationiOS/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  BWOnlineEducationiOS
+//
+//  Created by Bob Wong on 2017/6/4.
+//  Copyright © 2017年 Bob Wong Studio. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ BWOnlineEducationiOS/BWOnlineEducationiOS/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  BWOnlineEducationiOS
+//
+//  Created by Bob Wong on 2017/6/4.
+//  Copyright © 2017年 Bob Wong Studio. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
